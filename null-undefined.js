46b235e9b70a890b2a8d23e462f80f831c4b3087
S.m.Samrat <imsamrat24@gmail.com>
2021-02-01 22:31:44 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+ console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"Smart Dude", phone: 01716666};
+
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
---
+++ true.js
@@ -0,0 +1,6 @@
+if(){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
